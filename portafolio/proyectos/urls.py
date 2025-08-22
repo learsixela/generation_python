3d4178d0eef4b7baaa39b538a8eac716d56e503a
Israel <ipalma@gmail.com>
2025-08-22 12:07:10 -0400

    gurdado y listado de proyectos

--- portafolio/proyectos/urls.py
+++ portafolio/proyectos/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from proyectos import views
 
 urlpatterns = [
+    path('proyecto/listar/', views.listar_todos, name='listar_todos'),
     path('proyecto/crear/', views.crear, name='crear_projecto'),
+    
 ]
--- portafolio/proyectos/views.py
+++ portafolio/proyectos/views.py
@@ -1,12 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Proyecto 
 
 # Create your views here.
 #seleccionar todos
+def listar_todos(request):
+    #obtener todos los proyectos
+    proyectos = Proyecto.objects.all()
+    context = {
+        "proyectos": proyectos
+    }
+    return render(request, "listar_proyectos.html",context )
+    
 #crear
 def crear(request):
-    return render(request, "crear_proyecto.html" )
+    
+    if request.method == "GET":
+        #despliegue del formulario
+        print(request.GET) 
+        return render(request, "crear_proyecto.html" )
+
+    if request.method == "POST":
+        #capturar los parametros        
+        #print(request.POST)
+        #print(request.POST["nombre"])
+        #print(request.POST["descripcion"])
+        nombre = request.POST["nombre"]
+        descripcion= request.POST["descripcion"]
+        link = request.POST["link"]
+        
+        #variable que sera capturada/leida desde otro html 
+        context = {
+            "nombre" : nombre,
+            "descripcion":descripcion,
+            "link": link
+        }
+        
+        #guardar informacion en base datos
+        Proyecto.objects.create(nombre=nombre,descripcion=descripcion,link=link   )
+        
+        return render(request, "mostrar_proyecto.html",context )
+        #return redirect("/") 
+        
 #seleccionar 1
 #actualizar
 #eliminar
 
-
+{'csrfmiddlewaretoken': ['JXJtrwlBgoF7RvIJ9CbYUqTKeELKWTwcGuiiZRtpoIjcp1jMrusISiDug2gj5Lt7'], 
+ 'nombre': ['proyecto 2'], 
+ 'descripcion': ['yhujhrtstyawetgw']
+ }
