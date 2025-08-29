bdc4787e7b9f659f8e3d010a123bd8ddd7e81178
Israel <ipalma@gmail.com>
2025-08-29 12:32:02 -0400

    componentes en django

--- portafolio/login/urls.py
+++ portafolio/login/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from login import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('/home', views.home, name='home'),
     path('contacto/',views.contacto, name='contacto'),
 ]
--- portafolio/proyectos/urls.py
+++ portafolio/proyectos/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from proyectos import views
 
 urlpatterns = [
-    path('proyecto/', views.listar_todos, name='listar_todos'),
+    path('', views.listar_todos, name='listar_todos'),
     path('proyecto/crear/', views.crear, name='crear_projecto'),
     path('proyecto/<int:id>/editar/', views.editar, name='editar_projecto'),
     path('proyecto/<int:id>/eliminar/', views.eliminar, name='eliminar_projecto'),
